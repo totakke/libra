68d05133d1fbdb7e398618bb5924c698a3ac3def
Toshiki Takeuchi <roimisia@gmail.com>
2017-09-23 18:03:16 +0900

    Add bench-selector to lein-libra

--- lein-libra/src/leiningen/libra.clj
+++ lein-libra/src/leiningen/libra.clj
@@ -6,9 +6,20 @@
             [leiningen.core.eval :as eval]
             [leiningen.core.project :as project]))
 
+(defn- split-args
+  [args]
+  (let [[nses selectors] (split-with (complement keyword?) args)]
+    (loop [acc {} [selector & selectors] selectors]
+      (if (seq selectors)
+        (let [[args next] (split-with (complement keyword?) selectors)]
+          (recur (assoc acc selector args) next))
+        (if selector
+          (assoc acc selector ())
+          acc)))))
+
 (defn- parse-only
   [xs]
-  (->> xs
+  (->> (map str xs)
        (map (fn [s]
               (let [[ns* v] (string/split s #"/")]
                 [ns* v])))
@@ -23,9 +34,21 @@
        (into {})))
 
 (defn- parse-args
-  [args]
-  (if (= (first args) ":only")
-    {:only (parse-only (rest args))}))
+  [args project]
+  (let [given-selectors (split-args args)
+        project-selectors (merge {:all '(constantly true)
+                                  :only '(constantly true)}
+                                 (-> project :libra :bench-selectors))
+        selectors (->> given-selectors
+                       (keep (fn [[k v]]
+                               (if-let [selector (k project-selectors)]
+                                 [k (if (= k :only)
+                                      (parse-only v)
+                                      selector)])))
+                       (into {}))]
+    (if (seq selectors)
+      selectors
+      (select-keys project-selectors [:default]))))
 
 (defn- select-namespaces
   [namespaces selectors]
@@ -43,9 +66,14 @@
            (when (seq ~ns-sym)
              (apply require :reload ~ns-sym))
            (doseq [ns# ~ns-sym]
-             (if-let [vs# (get (:only '~selectors) ns#)]
-               (doseq [v# vs#] (libra.bench/bench-var (resolve v#)))
-               (libra.bench/bench-ns ns#))))))))
+             (if-let [only# (:only '~selectors)]
+               (if-let [vs# (get only# ns#)]
+                 (doseq [v# vs#] (libra.bench/bench-var (resolve v#)))
+                 (libra.bench/bench-ns ns#))
+               (binding [libra.bench/*selector* (->> (vals '~selectors)
+                                                     (map eval)
+                                                     (apply every-pred))]
+                 (libra.bench/bench-ns ns#)))))))))
 
 (defn libra
   "Measure the project's benchmarks.
@@ -55,9 +83,9 @@ example, `lein libra :only example.foo-bench` only runs benchmarks in the
 specified namespace."
   [project & args]
   (let [libra-profile (merge {:bench-paths ["bench"]} (:libra project))
-        project (project/merge-profiles project [{:source-paths (:bench-paths libra-profile)}
-                                                 {:libra libra-profile}])
-        selectors (parse-args args)
+        project (project/merge-profiles project [{:source-paths (:bench-paths libra-profile)}])
+        selectors (parse-args (map edn/read-string args) project)
+        _ (eval/prep project)
         namespaces (->> (:bench-paths libra-profile)
                         (mapcat (comp tns/find-namespaces-in-dir io/file))
                         distinct)
