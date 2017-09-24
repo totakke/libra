d09456e18994dec122e4dce1eed5ef587cbae307
Toshiki Takeuchi <roimisia@gmail.com>
2017-09-24 13:42:19 +0900

    Add examples of Criterium integration

--- example/bench/example/core_bench.clj
+++ example/bench/example/core_bench.clj
@@ -1,5 +1,6 @@
 (ns example.core-bench
   (:require [libra.bench :refer :all]
+            [libra.criterium :as c]
             [example.core :refer :all]))
 
 (defbench primes-with-trial-div-bench
@@ -7,3 +8,9 @@
 
 (defbench primes-with-eratosthenes-bench
   (is (dur 10 (doall (primes-with-eratosthenes 100000)))))
+
+(defbench primes-with-trial-div-bench-precise
+  (is (c/bench (doall (primes-with-trial-div 100000)))))
+
+(defbench primes-with-eratosthenes-bench-precise
+  (is (c/bench (doall (primes-with-eratosthenes 100000)))))
--- example/project.clj
+++ example/project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/totakke/libra"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[net.totakke/lein-libra "0.1.0"]]
-  :profiles {:dev {:dependencies [[net.totakke/libra "0.1.0"]]}}
+  :profiles {:dev {:dependencies [[criterium "0.4.4"]
+                                  [net.totakke/libra "0.1.0"]]}}
   :libra {:bench-paths ["bench"] ; default "bench"
           })
