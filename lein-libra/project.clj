d66c2f7977f98acf82620f8e0e3292b28d835e3b
Toshiki Takeuchi <roimisia@gmail.com>
2018-01-18 20:05:31 +0900

    lein-libra: prepare for next development iteration (0.1.2-SNAPSHOT)

--- lein-libra/project.clj
+++ lein-libra/project.clj
@@ -1,4 +1,4 @@
-(defproject net.totakke/lein-libra "0.1.1"
+(defproject net.totakke/lein-libra "0.1.2-SNAPSHOT"
   :description "Libra Leiningen plugin"
   :url "https://github.com/totakke/libra"
   :scm {:dir ".."}
