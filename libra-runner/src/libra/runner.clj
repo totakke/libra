88bcb765f62034623c5471034f111c0be8c962d8
Toshiki Takeuchi <roimisia@gmail.com>
2018-07-17 18:55:12 +0900

    Add runner

---
+++ libra-runner/src/libra/runner.clj
@@ -0,0 +1,43 @@
+(ns libra.runner
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.tools.cli :as cli]
+            [clojure.tools.namespace :as tns]
+            [libra.bench :as libra]))
+
+(defn- bench
+  [options]
+  (let [dirs #{"bench"}
+        namespaces (or (seq (:namespace options))
+                       (->> dirs
+                            (map io/file)
+                            (mapcat tns/find-namespaces-in-dir)
+                            distinct))]
+    (when (seq namespaces)
+      (apply require :reload namespaces))
+    (apply libra/run-benches namespaces)))
+
+(def cli-options
+  [["-n" "--namespace SYMBOL" "Symbol indicating a specific namespace to bench."
+    :parse-fn symbol
+    :assoc-fn (fn [m k v] (update-in m [k] (fnil conj #{}) v))]
+   ["-h" "--help"]])
+
+(defn- help
+  [summary]
+  (println "Usage: clj -m" (namespace `usage) "[options]\n")
+  (println summary))
+
+(defn- exit
+  [status message]
+  (println message)
+  (System/exit status))
+
+(defn -main
+  "Entry point for the libra runner"
+  [& args]
+  (let [{:keys [options errors summary]} (cli/parse-opts args cli-options)]
+    (cond
+      errors (exit 1 (string/join \newline errors))
+      (:help options) (help summary)
+      :else (bench options))))
