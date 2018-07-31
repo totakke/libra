39c29adc2133d1270044fd460973b6934ddc8fbe
Toshiki Takeuchi <roimisia@gmail.com>
2018-07-31 11:26:10 +0900

    Update lein-libra of example to 0.1.2

--- example/project.clj
+++ example/project.clj
@@ -2,7 +2,7 @@
   :description "Example of Libra"
   :url "https://github.com/totakke/libra"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[net.totakke/lein-libra "0.1.1"]]
+  :plugins [[net.totakke/lein-libra "0.1.2"]]
   :profiles {:dev {:dependencies [[criterium "0.4.4"]
                                   [net.totakke/libra "0.1.1"]]}}
   :libra {:bench-paths ["bench"] ; default "bench"
