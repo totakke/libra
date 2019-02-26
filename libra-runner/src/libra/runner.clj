6d2da78300438cb212b58d17675fc9e5d405ad49
Toshiki Takeuchi <roimisia@gmail.com>
2019-02-26 19:31:36 +0900

    libra-runner: add 'include' and 'exclude' options

--- libra-runner/src/libra/runner.clj
+++ libra-runner/src/libra/runner.clj
@@ -13,10 +13,23 @@
                        (->> dirs
                             (map io/file)
                             (mapcat tns/find-namespaces-in-dir)
-                            distinct))]
+                            distinct))
+        inclusion (if (:include options)
+                    (apply some-fn (:include options))
+                    (constantly true))
+        exclusion (if (:exclude options)
+                    (complement (apply some-fn (:exclude options)))
+                    (constantly true))]
     (when (seq namespaces)
       (apply require :reload namespaces))
-    (apply libra/run-benches namespaces)))
+    (binding [libra/*selector* (every-pred inclusion exclusion)]
+      (apply libra/run-benches namespaces))))
+
+(defn- parse-kw
+  [s]
+  (if (.startsWith s ":")
+    (read-string s)
+    (keyword s)))
 
 (defn- acc-opts
   [m k v]
@@ -27,6 +40,12 @@
     :assoc-fn acc-opts]
    ["-n" "--namespace SYMBOL" "Symbol indicating a specific namespace to run benchmarks."
     :parse-fn symbol
+    :assoc-fn acc-opts]
+   ["-i" "--include KEYWORD" "Run only benchmarks that have this metadata keyword."
+    :parse-fn parse-kw
+    :assoc-fn acc-opts]
+   ["-e" "--exclude KEYWORD" "Exclude benchmarks with this metadata keyword."
+    :parse-fn parse-kw
     :assoc-fn acc-opts]
    ["-h" "--help"]])
 
